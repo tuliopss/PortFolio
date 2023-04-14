b34f0b0d3e1562bb57999d15c940387df2c4fca0
Túlio <tuliovictorp@hotmail.com>
2023-04-14 18:07:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/index.js', express.static(path.join(__dirname, 'public/index.js'), {type: 'application/javascript'}));
+
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.listen(3000, (erro) => {
+    if(erro) {
+        console.log('Erro ao iniciar servidor');
+    } else {
+        console.log('Servidor iniciado');
+    }
+ })
+
+console.log('oi');
