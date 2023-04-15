ea1aaa4976855a3ae6923b61a77d4d672e8e9320
Túlio <tuliovictorp@hotmail.com>
2023-04-14 23:58:53 -0300

    correcoes e mais rotas

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/index.js', express.static(path.join(__dirname, 'public/index.js'), {type: 'application/javascript'}));
 
 
-
 app.get('/', (req, res) => {
     res.render('home')
 })
@@ -16,6 +15,27 @@ app.get('/', (req, res) => {
 app.get('/calculadora', (req, res) => {
     res.render('calculadora')
 })
+
+app.get('/todolist', (req, res) => {
+    res.render('toDoList')
+})
+
+app.get('/coffemenu', (req, res) => {
+    res.render('coffemenu')
+})
+app.get('/coffeshop', (req, res) => {
+    res.render('coffeshop')
+})
+app.get('/search', (req, res) => {
+    res.render('search')
+})
+app.get('/prova', (req, res) => {
+    res.render('prova')
+})
+app.get('/agenda', (req, res) => {
+    res.render('coffemenu')
+})
+
 
 app.listen(3000, (erro) => {
     if(erro) {
